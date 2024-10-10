e101b6ca628f00eb7e8537d3038beb83c5957ee6
M Nasir Khan <immnk97@gmail.com>
2024-10-10 23:45:41 +0500

    vite + tailwind files

---
+++ tailwind.config.js
@@ -0,0 +1,43 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// };
+
+// tailwind.config.js
+// export default {
+//   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// };
+
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: ["./index.html", "./src/**/*.{js,jsx}"], // Adjust this if you have TypeScript files too
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// };
+
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: ["./index.html", "./src/**/*.{js,jsx}"],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// };
